0c5c23d1485dadcfd560626330acd9ddb42d4de2
Jordan Gunther <jordankgunther@gmail.com>
2019-06-28 15:56:49 -0600

    used mapgetters to make it easier to use multiple getters

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
-        <p>Counter is: {{ counter }}</p>
-        <p>Number of Clicks: {{ clicks }}</p>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of Clicks: {{ stringCounter }}</p>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+
     export default {
-        computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
-            }
-        }
+        computed: mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])
     }
 </script>
