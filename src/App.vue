ecae9bea4b9c687ca15c23a3c6854f6f4cff3661
Jordan Gunther <jordankgunther@gmail.com>
2019-06-28 16:44:08 -0600

    set up custom two way binding to update the store

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
         <hr>
           <app-counter></app-counter>
           <app-another-counter></app-another-counter>
+          <hr>
+          <input type="text" :value="value" @input="updateValue">
+          <p>{{ value }}</p>
       </div>
     </div>
   </div>
@@ -20,6 +23,16 @@
   import AnotherCounter from './components/AnotherCounter.vue';
 
   export default {
+      computed: {
+          value() {
+              return this.$store.getters.value
+          }
+      },
+      methods: {
+          updateValue(event) {
+              this.$store.dispatch('updateValue', event.target.value);
+          }
+      },
       components: {
           appCounter: Counter,
           appResult: Result,
